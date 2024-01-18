2ae8a1339b0aab6e959ca922f5df8fd93fac9e29
Gustav <gurra@LAPTOP-V6IH5KCJ>
2024-01-18 12:08:57 +0100

    Ändrade till Hello Åke! istället för Hello World!

---
+++ StartupProject/src/main/java/se/hig/startupproject/StartupProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package se.hig.startupproject;
+
+/**
+ *
+ * @author gurra
+ */
+public class StartupProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Åke!");
+    }
+}
