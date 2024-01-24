10d0f760872417b34a42e62eade776b318d1c4df
Gustav <gurra@LAPTOP-V6IH5KCJ>
2024-01-24 14:08:39 +0100

    lab01

---
+++ lab01/src/main/java/se/hig/lab01/Lab01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package se.hig.lab01;
+
+/**
+ *
+ * @author gurra
+ */
+public class Lab01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
